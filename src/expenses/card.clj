104d27f3339e53a0b43072c70a323d4f8f674208
Eduardo Borges <eduardomrb@gmail.com>
2015-01-03 12:14:45 -0300

    Extend ofx-clj methods to be able to parse credit card transacntions

--- src/expenses/card.clj
+++ src/expenses/card.clj
@@ -1,7 +1,9 @@
 (ns expenses.card
   (:require [expenses.financial-record :refer :all]
             [ofx-clj.core :as ofx])
-  (:import [net.sf.ofx4j.domain.data.creditcard CreditCardResponseMessageSet]))
+  (:import [net.sf.ofx4j.domain.data.creditcard CreditCardResponseMessageSet
+                                                CreditCardStatementResponseTransaction
+                                                CreditCardStatementResponse]))
 
 (def the-file "resources/card/faturaCartao.ofx")
 
@@ -11,5 +13,22 @@
                   :type [.getType str]
                   :version .getVersion
                   :mesages [.getResponseMessages #(map ofx/parse-data %)]))
+
+(defmethod ofx/parse-data CreditCardStatementResponseTransaction
+  [transaction]
+  (ofx/obj-to-map transaction
+                  :message [.getMessage ofx/parse-data]
+                  :wrapped-message [.getWrappedMessage ofx/parse-data]))
+
+(defmethod ofx/parse-data CreditCardStatementResponse
+  [response]
+  (ofx/obj-to-map response
+                  :account [.getAccount ofx/parse-data]
+                  :available-balance [.getAvailableBalance ofx/parse-data]
+                  :currency-code .getCurrencyCode
+                  :ledger-balance [.getLedgerBalance ofx/parse-data]
+                  :marketing-info .getMarketingInfo
+                  :response-message-name .getResponseMessageName
+                  :transaction-list [.getTransactionList ofx/parse-data]))
 
 (ofx/parse the-file)
