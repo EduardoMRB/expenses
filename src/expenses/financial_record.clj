a01b50bc525a0d7a2376065bb738663eac78c226
Eduardo Borges <eduardomrb@gmail.com>
2015-01-08 15:33:06 -0300

    Date conversion is no longer needed in financial-record

--- src/expenses/financial_record.clj
+++ src/expenses/financial_record.clj
@@ -1,5 +1,4 @@
 (ns expenses.financial-record
-  (:require [clj-time.format :as f])
   (:import org.joda.time.DateTime))
 
 (defrecord FinancialRecord 
@@ -7,16 +6,6 @@
 
 (defn vec->FinancialRecord [expense]
   (apply ->FinancialRecord expense))
-
-(def br-format (f/formatter "dd/MM/YYYY"))
-
-(defmulti date class)
-
-(defmethod date FinancialRecord
-  [{date :date}]
-  (if (instance? DateTime date)
-    date
-    (f/parse br-format date)))
 
 (defn- sum-expenses-by 
   "Takes a predicate fn and a coll of FinancialRecord and return
@@ -26,11 +15,11 @@
 
 (defn income [records]
   (letfn [(income? [record]
-                  (let [excludes-descriptions #{"S A L D O" "Saldo Anterior"}]
-                    (and 
-                      (not (contains? excludes-descriptions
-                                      (:description record)))
-                      (pos? (:value record)))))]
+            (let [excludes-descriptions #{"S A L D O" "Saldo Anterior"}]
+              (and 
+                (not (contains? excludes-descriptions
+                                (:description record)))
+                (pos? (:value record)))))]
     (sum-expenses-by income? records)))
 
 (defn debt [records]
--- test/expenses/financial_record_test.clj
+++ test/expenses/financial_record_test.clj
@@ -3,31 +3,10 @@
             [expenses.financial-record :refer :all]
             [clj-time.core :as t :refer [date-time]]))
 
-(def string-date-record 
-  (map->FinancialRecord {:date "10/11/2014"
-                         :origin "Origem"
-                         :description "Desc"
-                         :balance-date "11/11/2014"
-                         :doc-number 213231
-                         :value 11.23}))
-
-(def date-time-record 
-  (map->FinancialRecord {:date (date-time 2014 11 11)
-                         :origin "Origem"
-                         :description "Desc"
-                         :balance-date "11/11/2014"
-                         :doc-number 213231
-                         :value 11.23}))
 (def test-records 
   [(->FinancialRecord "05/30/2014" "" "Saldo Anterior" "" "0" 111.87M)
    (->FinancialRecord "06/02/2014" "2960-2" "Compra com Cartão - 02/06 21:50    KILO BYTE" "" "178653" -4.20M)
    (->FinancialRecord "06/02/2014" "2960-2" "Compra com Cartão - 31/05 23:11 RIA ROSARIO" "" "183473" -15.75M)])
-
-(deftest financial-record-date
-  (testing "date function get date from string"
-    (is (= (date string-date-record) (date-time 2014 11 10))))
-  (testing "if it's a DateTime object, just return it"
-    (is (= (date date-time-record) (date-time 2014 11 11)))))
 
 (deftest sum-of-income-entries
   (testing "it sums the incomes value correctly"
