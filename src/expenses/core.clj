fa29fe8953c8f0362aedc7f42be97eaa801f9c0e
Eduardo Borges <eduardomrb@gmail.com>
2015-01-12 20:58:51 -0300

    Require expenses.financial-record from core

--- src/expenses/core.clj
+++ src/expenses/core.clj
@@ -1,6 +1,7 @@
 (ns expenses.core
   (:require [clojure.java.io :as io]
-            [expenses.parser :refer :all]))
+            [expenses.parser :refer :all]
+            [expenses.financial-record :refer :all]))
 
 (defn file? [file]
   (.isFile file))
