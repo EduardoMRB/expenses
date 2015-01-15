902500830cfacef9e73fecfa60966a3af8dce589
Eduardo Borges <eduardomrb@gmail.com>
2015-01-15 11:11:16 -0300

    Change datomic-api version and setup configuration to start datomic via lein-datomic plugin

--- project.clj
+++ project.clj
@@ -7,8 +7,23 @@
                  [net.sf.ofx4j/ofx4j "1.6"]
                  [cc.artifice/ofx-clj "0.1"]
                  [clj-time "0.9.0"]
-                 [com.datomic/datomic-free "0.9.5078" :exclusions [joda-time]]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]}}
+
+                 [com.datomic/datomic-pro "0.9.5078" :exclusions [joda-time]]
+
+                 [io.pedestal/pedestal.service "0.3.1"]
+                 [io.pedestal/pedestal.jetty "0.3.1"]
+
+                 [ch.qos.logback/logback-classic "1.1.2" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.7"]
+                 [org.slf4j/jcl-over-slf4j "1.7.7"]
+                 [org.slf4j/log4j-over-slf4j "1.7.7"]]
+  :resource-paths ["config" "resources"]
+  :datomic {:schemas ["resources/datomic/schema.edn"]}
+  :profiles {:dev {:dependencies [[midje "1.6.3"]
+                                  [io.pedestal/pedestal.service-tools "0.3.1"]]
+                   :aliases {"run-dev" ["trampoline" "run" "-m" "expenses.server/run-dev"]}
+                   :datomic {:config "resources/datomic/dev-transactor-template.properties"
+                             :db-uri "datomic:dev://localhost:4334/expenses"}}}
   :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
                                    :creds :gpg}}
-  :main expenses.core)
+  :main ^{:skip-aot true} expenses.server)
